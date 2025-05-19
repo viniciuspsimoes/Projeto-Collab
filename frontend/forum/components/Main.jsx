3a6970f011e27e7a2fe82e3190b0f4ce1ba1146a
Gabriela Porto <mg.portopes@gmail.com>
2025-05-19 15:07:45 -0300

    Create Main.jsx
    Componente principal da tela de início do site

---
+++ frontend/forum/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+
+const Main = () => {
+  return <div className="main">
+
+        <div className='item-list'>
+            <div className='item-list__header'>
+                <h2>Posts</h2>
+            </div>
+        </div>
+    </div>
+};
+
+export default Main;
