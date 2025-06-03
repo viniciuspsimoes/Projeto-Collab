0c123d5b46f1b29054a16a5ae36d3594bbe4a17e
Vinicius Simões <107807798+viniciuspsimoes@users.noreply.github.com>
2025-06-03 19:27:36 -0300

    Update PostCreator.jsx
    alteração para interagir com o chatbotpopup

--- frontend/forum/src/components/PostCreator.jsx
+++ frontend/forum/src/components/PostCreator.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import { faArrowRightToBracket } from '@fortawesome/free-solid-svg-icons';
-import { faPaperclip } from '@fortawesome/free-solid-svg-icons';
-import { faRobot } from '@fortawesome/free-solid-svg-icons';
-
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightToBracket, faPaperclip, faRobot } from '@fortawesome/free-solid-svg-icons';
+import ChatBotPopUp from "./ChatBotPopUp";
 export default function PostCreator() {
+  const [showChatBot, setShowChatBot] = useState(false);
+
   return (
     <div className="post-creator">
       <h2>Faça uma postagem</h2>
-      <textarea placeholder="Descreva sua ideia..."/>
+      <textarea placeholder="Descreva sua ideia..." />
       <input type="text" placeholder="Coloque suas #tags aqui, separadas por vírgula..." />
       <div className="buttons">
-        
-        {/*Botão para interação com IA*/}
-        <button title='Usar IA'><FontAwesomeIcon icon={faRobot} /></button>
-        
-        <button title='Anexar imagem'><FontAwesomeIcon icon={faPaperclip} /></button>
-        <button title='Postar'><FontAwesomeIcon icon={faArrowRightToBracket}/></button>
-      
+        <button title='Usar IA' onClick={() => setShowChatBot(true)}>
+          <FontAwesomeIcon icon={faRobot} />
+        </button>
+        <button title='Anexar imagem'>
+          <FontAwesomeIcon icon={faPaperclip} />
+        </button>
+        <button title='Postar'>
+          <FontAwesomeIcon icon={faArrowRightToBracket} />
+        </button>
       </div>
+      {showChatBot && <ChatBotPopUp onClose={() => setShowChatBot(false)} />}
     </div>
   );
 }
